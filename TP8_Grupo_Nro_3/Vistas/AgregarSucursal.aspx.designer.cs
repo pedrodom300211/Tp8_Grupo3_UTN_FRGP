076e8aade9407f3529b5f3f872cfe5073ca7498a
unknown <pedrodinguez2003@gmail.com>
2023-06-01 10:01:00 -0300

    Subo segundo aporte del ejercicio 1

--- TP8_Grupo_Nro_3/Vistas/AgregarSucursal.aspx.designer.cs
+++ TP8_Grupo_Nro_3/Vistas/AgregarSucursal.aspx.designer.cs
@@ -49,5 +49,14 @@ namespace Vistas
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink hlbtnEliminarSucursal;
+
+        /// <summary>
+        /// Control txtNombreSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreSucursal;
     }
 }
